be94ab047f58495a29359a329bfc39ae2c5d8319
Shin_jihwan <rocrie12@naver.com>
2021-11-02 02:16:51 +0900

    first commit

--- level1/crane_claw_machine(kakao).py
+++ level1/crane_claw_machine(kakao).py
@@ -1,3 +1,5 @@
+# 초기 랜덤으로 주어진 인형들이 있는 2차원 배열 board
+# 크레인이 움직인 위치 moves
 def solution(board, moves):
     stacklist = []
     answer = 0
@@ -10,8 +12,8 @@ def solution(board, moves):
 
                 if len(stacklist) > 1:
                     if stacklist[-1] == stacklist[-2]:
+                        stacklist.pop(-1)  # 맨위꺼 삭제*2
                         stacklist.pop(-1)
-                        stacklist.pop(-1)
-                        answer += 2
+                        answer += 2  # 터진 인형 갯수 추가
                 break
     return answer
