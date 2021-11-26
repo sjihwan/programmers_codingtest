3a915d48842ee4e9db5964e19f75c59e07dc4e25
Shin_jihwan <rocrie12@naver.com>
2021-11-27 00:24:09 +0900

    Update 숫자 표현.py

---
+++ level2/숫자 표현.py
@@ -0,0 +1,13 @@
+def solution(n):
+    count = 0
+
+    for i in range(1, n + 1):  # 1부터 n까지
+        total = 0
+        for j in range(i, n + 1):
+            total += j
+            if total == n:
+                count += 1
+                break
+            elif total > n:
+                break
+    return count
