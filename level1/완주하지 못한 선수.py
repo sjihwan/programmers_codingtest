fe2e560d429cc4def52246dc52bf4dc743c5cf1c
sjihwan <rocrie12@naver.com>
2022-03-19 12:56:39 +0900

    update

--- level1/완주하지 못한 선수.py
+++ level1/완주하지 못한 선수.py
@@ -1,15 +1,12 @@
-# Hash (Key, Value)
 def solution(participant, completion):
     hashDict = {}
     sumHash = 0
-    # 1. participant list의 hash를 구하고, hash 값을 더한다.
+    
     for part in participant:
-        hashDict[hash(part)] = part # hashDict[Key값] = value값
+        hashDict[hash(part)] = part
         sumHash += hash(part)
 
-    # 2. completion list의 hash를 빼준다.
     for comp in completion:
         sumHash -= hash(comp)
 
-    # 3. 남은 값이 완주하지 못한 선수의 hash 값이 된다.
     return hashDict[sumHash]
